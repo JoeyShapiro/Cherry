c9f12785d72041aa484bd6c5ba7033c2dec28ddb
JoeyShapiro <joeyashapiro@gmail.com>
2023-12-01 15:16:21 -0600

    now use templates

--- server/app.py
+++ server/app.py
@@ -1,8 +1,5 @@
 from flask import Flask, render_template
 
-with open('index.html') as f:
-    page_index = f.read()
-
 app = Flask(__name__)
 
 @app.route('/test')
@@ -11,11 +8,11 @@ def hello_world():
 
 @app.route('/')
 def index():
-    # TODO allows for live editing
-    with open('index.html') as f:
-        page_index = f.read()
+    return render_template('index.html')
 
-    return page_index
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0" , port=8000)
