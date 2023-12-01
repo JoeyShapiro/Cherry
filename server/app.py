ae2c7b53c101a18a0fa4171f5360089b2eecd8a0
JoeyShapiro <joeyashapiro@gmail.com>
2023-12-01 16:37:28 -0600

    checkpoint

--- server/app.py
+++ server/app.py
@@ -2,17 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/test')
-def hello_world():
-    return render_template('index.html')
-
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template('login.html')
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0" , port=8000)
+    app.run(host="0.0.0.0" , port=8000, debug=True)
